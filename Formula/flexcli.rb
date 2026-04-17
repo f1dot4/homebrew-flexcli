5e6773e8e5fc1dfd8bb61ad35a077b81e0221ee0
Lukas Leidinger <lukas.leidinger@willhaben.at>
2026-04-17 13:39:46 +0200

    chore: release v0.2.8

--- Formula/flexcli.rb
+++ Formula/flexcli.rb
@@ -1,8 +1,8 @@
 class Flexcli < Formula
   desc "Management CLI for FlexCoach AI fitness platform"
   homepage "https://github.com/f1dot4/homebrew-flexcli"
-  url "https://github.com/f1dot4/homebrew-flexcli/archive/refs/tags/v0.2.7.tar.gz"
-  sha256 "34bbdf49818ec458aec741b4a1800d8a2ae53af15052dcf09ee1ce7e23c06af0"
+  url "https://github.com/f1dot4/homebrew-flexcli/archive/refs/tags/v0.2.8.tar.gz"
+  sha256 "PLACEHOLDER"
   license "MIT"
 
   depends_on "go" => :build
--- cmd/flexcli/main.go
+++ cmd/flexcli/main.go
@@ -16,7 +16,7 @@ var (
 	contextName string
 	rootCfg     *config.Config
 	resolvedCtx config.Context
-	Version     = "0.2.7"
+	Version     = "0.2.8"
 )
 
 var rootCmd = &cobra.Command{
