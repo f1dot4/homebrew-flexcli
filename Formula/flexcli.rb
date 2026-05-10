50c0cffcdd25bd1e293cadcf01db8b60b99a473f
Lukas Leidinger <lukas.leidinger@willhaben.at>
2026-05-10 22:05:28 +0200

    chore: release v0.2.47

--- Formula/flexcli.rb
+++ Formula/flexcli.rb
@@ -1,8 +1,8 @@
 class Flexcli < Formula
   desc "Management CLI for FlexCoach AI fitness platform"
   homepage "https://github.com/f1dot4/homebrew-flexcli"
-  url "https://github.com/f1dot4/homebrew-flexcli/archive/refs/tags/v0.2.46.tar.gz"
-  sha256 "94feae6852e8d5b8bfdbda987f0402683561c5b289250f422e7a80b6d0fb833f"
+  url "https://github.com/f1dot4/homebrew-flexcli/archive/refs/tags/v0.2.47.tar.gz"
+  sha256 "PLACEHOLDER"
   license "MIT"
 
   depends_on "go" => :build
